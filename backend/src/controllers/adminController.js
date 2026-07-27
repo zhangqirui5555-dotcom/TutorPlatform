6c3963ea1883b560aa1d2b0f01b53c07096d4b0d
zhangqirui5555-dotcom <zhangqirui5555@gmail.com>
2026-07-27 20:33:12 +0800

    feat: add admin module (backend/src/controllers/adminController.js)

---
+++ backend/src/controllers/adminController.js
@@ -0,0 +1,33 @@
+const adminService = require("../services/adminService")
+
+async function getUsers(req, res) {
+  const users = await adminService.getUsers(req.query || {})
+  res.json({ users })
+}
+
+async function updateUserStatus(req, res) {
+  const user = await adminService.updateUserStatus(
+    Number(req.params.id),
+    req.body?.status,
+    req.user.id,
+  )
+  res.json({ user })
+}
+
+async function getGovernanceOverview(req, res) {
+  const overview = await adminService.getGovernanceOverview()
+  res.json(overview)
+}
+
+async function closeDemand(req, res) {
+  const demand = await adminService.closeDemand(Number(req.params.id))
+  res.json({ demand })
+}
+
+module.exports = {
+  closeDemand,
+  getGovernanceOverview,
+  getUsers,
+  updateUserStatus,
+}
+
